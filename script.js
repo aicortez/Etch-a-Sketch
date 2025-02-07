b57dfc47e4191ed6672bd09898494aa32e8cbd75
Alejandro Cortez <dev@sk1ll.cl>
2025-02-07 12:24:49 -0300

    Dynamically updates grid and pixel size

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const container = document.querySelector(".container");
 
-let gridX = 16
-let gridY = 16
+let pixelSize = 5
+let gridX = 24
+let gridY = 34
 let gridSize = gridX*gridY;
 for (let i = 0; i < gridSize; i++) {
     let grid = document.createElement("div")
     container.appendChild(grid)
-    document.querySelector("div").appendChild(grid)
+    document.querySelector(".container").appendChild(grid)
 }
 
-container.style.width = `${gridX*gridY}px`
-container.style.height = `${gridX*gridY}px`
+container.style.width = `${pixelSize*gridX}px`
+container.style.height = `${pixelSize*gridY}px`
 
 const fullGrid = document.querySelectorAll(".container div")
 fullGrid.forEach(item => {
     item.className = "grid"
-    item.style.width = `${gridX}px`
-    item.style.height = `${gridY}px`
+    item.style.width = `${pixelSize}px`
+    item.style.height = `${pixelSize}px`
     item.addEventListener("mouseover", () => {
         item.style.backgroundColor = "purple"
     })
 })
+
