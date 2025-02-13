8e2e86d02d3ba32ec269735e8828e98504871982
Alejandro Cortez <dev@sk1ll.cl>
2025-02-12 22:22:22 -0300

    Adds button to create new grid

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container")
 
-let pixelSize = 5
-let gridX = 24
-let gridY = 34
+let pixelSize = 8
+let gridX = 16
+let gridY = 16
 let gridSize = gridX*gridY;
-for (let i = 0; i < gridSize; i++) {
-    let grid = document.createElement("div")
-    container.appendChild(grid)
-    document.querySelector(".container").appendChild(grid)
+
+function clearGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
 }
 
-container.style.width = `${pixelSize*gridX}px`
-container.style.height = `${pixelSize*gridY}px`
-
-const fullGrid = document.querySelectorAll(".container div")
-fullGrid.forEach(item => {
-    item.className = "grid"
-    item.style.width = `${pixelSize}px`
-    item.style.height = `${pixelSize}px`
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "purple"
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        let grid = document.createElement("div")
+        container.appendChild(grid)
+        document.querySelector(".container").appendChild(grid)
+    }
+    container.style.width = `${pixelSize*gridX}px`
+    container.style.height = `${pixelSize*gridY}px`
+    const fullGrid = document.querySelectorAll(".container div")
+    fullGrid.forEach(item => {
+        item.className = "grid"
+        item.style.width = `${pixelSize}px`
+        item.style.height = `${pixelSize}px`
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "purple"
+        })
     })
-})
+}
 
+createGrid()
+
+
+
+
+// function populateGrid() {
+//     container.style.width = `${pixelSize*gridX}px`
+//     container.style.height = `${pixelSize*gridY}px`
+//     const fullGrid = document.querySelectorAll(".container div")
+//     fullGrid.forEach(item => {
+//     item.className = "grid"
+//     item.style.width = `${pixelSize}px`
+//     item.style.height = `${pixelSize}px`
+//     item.addEventListener("mouseover", () => {
+//         item.style.backgroundColor = "purple"
+//     })
+// })
+// }
+// populateGrid()
+
+const newGridButton = document.querySelector(".new-grid")
+newGridButton.addEventListener("click", () => {
+    clearGrid()
+    gridX = prompt("Please enter width size:")
+    while (gridX > 100 || gridX < 1) {
+        gridX = prompt("Width must be between 1 and 100:")
+    }
+    gridY = prompt("Please enter length size:")
+    while (gridY > 100 || gridY < 1) {
+        gridY = prompt("Length must be between 1 and 100:")
+    }
+    gridSize = gridX*gridY
+    createGrid()
+})
